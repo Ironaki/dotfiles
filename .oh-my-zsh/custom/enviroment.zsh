6328a305a8f32208e41e02cc6e5556c29a4110e4
Zixuan Li <armstrong0220@gmail.com>
2018-12-25 13:37:26 -0500

    add squirrel files

---
+++ .oh-my-zsh/custom/enviroment.zsh
@@ -0,0 +1,8 @@
+export PATH="/Users/Armstrong/anaconda/envs/ipykernel_py2/bin:$PATH"
+export PATH="/Users/Armstrong/anaconda/bin:$PATH"
+export PATH="$PATH:/usr/local/smlnj/bin"
+export PATH="$PATH:/Users/Armstrong/.util_script/shell"
+export PATH="$PATH:/Users/Armstrong/.util_script/python"
+
+export EDITOR="emacs"
+
