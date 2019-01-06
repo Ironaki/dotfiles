9116103992e9a39c849fce69776dabeba43be20f
Zixuan Li <armstrong0220@gmail.com>
2019-01-05 19:54:04 -0500

    Add prelude emacs config

--- .oh-my-zsh/custom/enviroment.zsh
+++ .oh-my-zsh/custom/enviroment.zsh
@@ -3,6 +3,7 @@ export PATH="/Users/Armstrong/anaconda/bin:$PATH"
 export PATH="$PATH:/usr/local/smlnj/bin"
 export PATH="$PATH:/Users/Armstrong/.util_script/shell"
 export PATH="$PATH:/Users/Armstrong/.util_script/python"
+export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
 
 export EDITOR="emacs"
 
