a8e88d54c129e83a8ea66bf5e249cbbe9a00fb98
Zixuan Li <armstrong0220@gmail.com>
2019-09-25 14:55:24 +0900

    better naming; add local template

--- .util_script/python/src/update.py
+++ .util_script/python/src/update.py
@@ -12,9 +12,9 @@ from os import chmod
 # Change this variable, if you add or delete files
 FILES = ["vocab", "lcinit", "csv2ledger_preprocess"]
 
-for file in FILES:
-    old_file = file+".py"
-    new_file = "../"+file
+for file_name in FILES:
+    old_file = file_name+".py"
+    new_file = "../"+file_name
     copyfile(old_file, new_file)
     chmod(new_file, 0o755)
     chmod(old_file, 0o644)
