873634819f1d6303f8cb1e93a9c52c4c4adb686a
Zixuan Li <armstrong0220@gmail.com>
2020-12-29 01:52:10 +0900

    Add docker config and update script
    - Clean up environment.zsh
    
    Signed-off-by: Zixuan Li <armstrong0220@gmail.com>

--- .oh-my-zsh/custom/enviroment.zsh
+++ .oh-my-zsh/custom/enviroment.zsh
@@ -1,22 +1,4 @@
 export PATH="$PATH:/usr/local/smlnj/bin"
 export PATH="$PATH:$HOME/.util_script/shell"
 export PATH="$PATH:$HOME/.util_script/python"
-# export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-export PATH="/usr/local/anaconda3/bin:$PATH"
-
-export EDITOR="emacs"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
-#         . "/usr/local/anaconda3/etc/profile.d/conda.sh"
-#     else
-#         export PATH="/usr/local/anaconda3/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
-# <<< conda initialize <<<
+export EDITOR="emacs --with-profile doom -nw"
