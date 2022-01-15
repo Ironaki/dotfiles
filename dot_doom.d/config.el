7815f238c61575eae81f57f04085ea7651942a7b
Zixuan Li <armstrong0220@gmail.com>
2022-01-15 11:23:53 +0900

    Add init.el for doom
    - Set relative line number for doom

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'solarized-light)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -60,6 +60,10 @@
       ns-option-modifier        'none
       mac-right-option-modifier 'meta
       ns-right-option-modifier  'meta)
+
+(setq doom-theme 'solarized-light)
+
+(setq display-line-numbers-type 'relative)
 
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 ;; END OF MY CONFIGURATION
