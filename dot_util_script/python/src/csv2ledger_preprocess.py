c6f331fe1f1d73a822d32a8ec8c60850cacf3686
Zixuan Li <armstrong0220@gmail.com>
2022-01-17 10:34:59 +0900

    Update ledger preprocess script

--- dot_util_script/python/src/csv2ledger_preprocess.py
+++ dot_util_script/python/src/csv2ledger_preprocess.py
@@ -26,38 +26,43 @@ import re
 
 
 def read_file(file_path, encoding="utf-8"):
-    """ Read the file, return list of lines """
+    """Read the file, return list of lines"""
     with open(file_path, encoding=encoding) as f:
         text_list = f.readlines()
     return text_list
 
 
 def write_file(file_path, text_list, encoding="utf-8"):
-    """ Write the file from list of lines  """
+    """Write the file from list of lines"""
     with open(file_path, encoding=encoding, mode="w") as f:
         f.writelines(text_list)
 
 
 def amazon_process(file_path):
-    """ Preprocess AmazonCard csv. Overwrite the file """
+    """Preprocess AmazonCard csv. Overwrite the file"""
     text_list = read_file(file_path, "shift-jis")
+    for i, text in enumerate(text_list):
+        words = text.split(",")
+        if words[1] == "ZWIFT":
+            words[2] = words[3]
+            text_list[i] = ",".join(words)
     write_file(file_path, text_list[:-3])  # last 3 lines irrelevant
 
 
 def rakutencard_process(file_path):
-    """ Preprocess RakutenCard csv, overwrite the file """
+    """Preprocess RakutenCard csv, overwrite the file"""
     text_list = read_file(file_path)
     write_file(file_path, text_list)
 
 
 def shinsei_process(file_path):
-    """ Process Shinsei csv, overwrite the file """
+    """Process Shinsei csv, overwrite the file"""
     text_list = read_file(file_path, "utf-16")  # Uncommon encoding
     write_file(file_path, text_list[8:])
 
 
 def mitsui_process(file_path):
-    """ Process Mitsui csv, overwrite the file """
+    """Process Mitsui csv, overwrite the file"""
     text_list = read_file(file_path, "shift-jis")
     # Sample line start :R01.08.01
     # line_start = re.compile("^R(\d\d)")
@@ -73,24 +78,26 @@ def mitsui_process(file_path):
 
 
 def rakutenbank_process(file_path):
-    """ Process RakutenBank csv, overwirte the file """
+    """Process RakutenBank csv, overwirte the file"""
     text_list = read_file(file_path, "shift-jis")
     write_file(file_path, text_list)
 
 
 def alipay_process(file_path):
-    """ Process AliPay csv, overwrite the file"""
+    """Process AliPay csv, overwrite the file"""
     text_list = read_file(file_path, "gb18030")
     write_file(file_path, text_list[4:-7])
 
 
 def wechat_process(file_path):
-    """ Process WeChat csv, overwrite the file """
+    """Process WeChat csv, overwrite the file"""
     text_list = read_file(file_path)
     text_list = text_list[16:]
     text_list = [line.replace("¥", "") for line in text_list]  # Take out ¥ sign
     # Edge Cases for wechat, What's wrong with Tencent engineers...
-    text_list = [line.replace("TSURUHA CO.,LTD", "TSURUHA CO.LTD") for line in text_list]
+    text_list = [
+        line.replace("TSURUHA CO.,LTD", "TSURUHA CO.LTD") for line in text_list
+    ]
     for i, line in enumerate(text_list):
         # Make income negative compare to expense
         text_list[i] = line.replace("收入,", "收入,-")
@@ -108,20 +115,25 @@ def wechat_process(file_path):
 
 def arg_parser():
     desc = "Preprocess csv files to be used in icsv2ledger"
-    parser = argparse.ArgumentParser(prog="Ledger Preprocessor",
-                                     description=desc)
-    parser.add_argument("file_path",
-                        nargs="+",
-                        metavar="FILE",
-                        type=str)
+    parser = argparse.ArgumentParser(prog="Ledger Preprocessor", description=desc)
+    parser.add_argument("file_path", nargs="+", metavar="FILE", type=str)
     # Required method for processing the csv file
     # each flag in choices corresponds to one *_process function
-    parser.add_argument("-m", "--method",
-                        choices=["amazon", "rakutencard",
-                                 "shinsei", "mitsui", "rakutenbank",
-                                 "alipay", "wechat"],
-                        required=True,
-                        help="choose a method of preprocessing")
+    parser.add_argument(
+        "-m",
+        "--method",
+        choices=[
+            "amazon",
+            "rakutencard",
+            "shinsei",
+            "mitsui",
+            "rakutenbank",
+            "alipay",
+            "wechat",
+        ],
+        required=True,
+        help="choose a method of preprocessing",
+    )
     args = parser.parse_args()
     return args
 
